defd34c8377c2293ff69d00d8d62c632c14b9b99
Virag Kormoczy <vkormoczy@gmail.com>
2022-03-14 15:12:06 +0200

    Fix from reviews

--- script.js
+++ script.js
@@ -1,58 +1,62 @@
-const bookSection = document.querySelector('.book-list');
-const bookTitle = document.querySelector('#title');
-const bookAuthor = document.querySelector('#author');
-const addBtn = document.querySelector('#submit');
+window.addEventListener("DOMContentLoaded", () => {
+  const bookSection = document.querySelector(".book-list");
+  const bookTitle = document.querySelector("#title");
+  const bookAuthor = document.querySelector("#author");
+  const addBtn = document.querySelector("#submit");
 
-const bookObjects = JSON.parse(localStorage.getItem('book-collection'));
-/* eslint-disable */
-function deleteBook() {
-  [...document.querySelectorAll(".deletebtn")].map((element) => {
-    const elementIndex = parseInt(element.getAttribute("data"), 10);
-    element.addEventListener("click", () => {
-      bookObjects.splice(elementIndex, 1);
-      localStorage.setItem("book-collection", JSON.stringify(bookObjects));
-      createBook();
+  const bookObjects = JSON.parse(localStorage.getItem("book-collection")) || [];
+  /* eslint-disable */
+  function deleteBook() {
+    [...document.querySelectorAll(".deletebtn")].map((element) => {
+      const elementIndex = parseInt(element.getAttribute("data"), 10);
+      element.addEventListener("click", () => {
+        bookObjects.splice(elementIndex, 1);
+        localStorage.setItem("book-collection", JSON.stringify(bookObjects));
+        createBook();
+      });
     });
-  });
-}
-
-function createBook() {
-  bookSection.innerHTML = "";
-  for (let i = 0; i < bookObjects.length; i += 1) {
-    const bookContainer = document.createElement("div");
-    const title = document.createElement("h2");
-    const author = document.createElement("h2");
-    const deleteBtn = document.createElement("button");
-    deleteBtn.setAttribute("class", "deletebtn");
-    deleteBtn.setAttribute("data", i);
-
-    title.textContent = bookObjects[i].title;
-    author.textContent = bookObjects[i].author;
-    deleteBtn.textContent = "Delete";
-
-    bookContainer.appendChild(title);
-    bookContainer.appendChild(author);
-    bookContainer.appendChild(deleteBtn);
-    bookSection.appendChild(bookContainer);
   }
-  deleteBook();
-}
-/* eslint-enable */
-createBook();
 
-function addBook(bookTitle, bookAuthor) {
-  const obj = {};
-  obj.title = bookTitle.value;
-  obj.author = bookAuthor.value;
-  bookObjects.push(obj);
+  function createBook() {
+    bookSection.innerHTML = "";
+    for (let i = 0; i < bookObjects.length; i += 1) {
+      const bookContainer = document.createElement("div");
+      const title = document.createElement("h2");
+      const author = document.createElement("h2");
+      const deleteBtn = document.createElement("button");
+      deleteBtn.setAttribute("class", "deletebtn");
+      deleteBtn.setAttribute("data", i);
+
+      title.textContent = bookObjects[i].title;
+      author.textContent = bookObjects[i].author;
+      deleteBtn.textContent = "Delete";
+
+      bookContainer.appendChild(title);
+      bookContainer.appendChild(author);
+      bookContainer.appendChild(deleteBtn);
+      bookSection.appendChild(bookContainer);
+    }
+    deleteBook();
+  }
+  /* eslint-enable */
   createBook();
-}
 
-addBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-  if (bookTitle.value === '' || bookAuthor.value === '') {
-    return;
+  function addBook(bookTitle, bookAuthor) {
+    const obj = {};
+    obj.title = bookTitle.value;
+    obj.author = bookAuthor.value;
+    bookObjects.push(obj);
+    createBook();
   }
-  addBook(bookTitle, bookAuthor);
-  localStorage.setItem('book-collection', JSON.stringify(bookObjects));
+
+  addBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (bookTitle.value === "" || bookAuthor.value === "") {
+      return;
+    }
+    addBook(bookTitle, bookAuthor);
+    localStorage.setItem("book-collection", JSON.stringify(bookObjects));
+    bookTitle.value = "";
+    bookAuthor.value = "";
+  });
 });
