a17211daf4f61bc5af501590563ffbb9ae112674
Virag Kormoczy <vkormoczy@gmail.com>
2022-03-14 13:35:59 +0200

    Add local storage and form validation

--- script.js
+++ script.js
@@ -1,33 +1,23 @@
-const bookSection = document.querySelector('.book-list');
-const bookTitle = document.querySelector('#title');
-const bookAuthor = document.querySelector('#author');
-const addBtn = document.querySelector('#submit');
-/* const deleteBtnDOM = [...document.querySelectorAll('button')]; */
+const bookSection = document.querySelector(".book-list");
+const bookTitle = document.querySelector("#title");
+const bookAuthor = document.querySelector("#author");
+const addBtn = document.querySelector("#submit");
 
-const bookObjects = [
-  {
-    title: 'Book1',
-    author: 'author1',
-  },
-  {
-    title: 'Book2',
-    author: 'author2',
-  },
-];
+const bookObjects = JSON.parse(localStorage.getItem("book-collection"));
 
 function createBook() {
-  bookSection.innerHTML = '';
+  bookSection.innerHTML = "";
   for (let i = 0; i < bookObjects.length; i += 1) {
-    const bookContainer = document.createElement('div');
-    const title = document.createElement('h2');
-    const author = document.createElement('h2');
-    const deleteBtn = document.createElement('button');
-    deleteBtn.setAttribute('class', 'deletebtn');
-    deleteBtn.setAttribute('data', i);
+    const bookContainer = document.createElement("div");
+    const title = document.createElement("h2");
+    const author = document.createElement("h2");
+    const deleteBtn = document.createElement("button");
+    deleteBtn.setAttribute("class", "deletebtn");
+    deleteBtn.setAttribute("data", i);
 
     title.textContent = bookObjects[i].title;
     author.textContent = bookObjects[i].author;
-    deleteBtn.textContent = 'Delete';
+    deleteBtn.textContent = "Delete";
 
     bookContainer.appendChild(title);
     bookContainer.appendChild(author);
@@ -47,16 +37,21 @@ function addBook(bookTitle, bookAuthor) {
   createBook();
 }
 
-addBtn.addEventListener('click', (e) => {
+addBtn.addEventListener("click", (e) => {
   e.preventDefault();
+  if (bookTitle.value === "" || bookAuthor.value === "") {
+    return;
+  }
   addBook(bookTitle, bookAuthor);
+  localStorage.setItem("book-collection", JSON.stringify(bookObjects));
 });
 
 function deleteBook() {
-  [...document.querySelectorAll('.deletebtn')].map((element) => {
-    const elementIndex = parseInt(element.getAttribute('data'));
-    element.addEventListener('click', () => {
+  [...document.querySelectorAll(".deletebtn")].map((element) => {
+    const elementIndex = parseInt(element.getAttribute("data"));
+    element.addEventListener("click", () => {
       bookObjects.splice(elementIndex, 1);
+      localStorage.setItem("book-collection", JSON.stringify(bookObjects));
       createBook();
     });
   });
