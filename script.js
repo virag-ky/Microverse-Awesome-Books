7fb89872e668671d2e3c47cdda82a953b25c6509
Virag Kormoczy <vkormoczy@gmail.com>
2022-03-15 12:36:39 +0200

    Fix linter errors

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const bookSection = document.querySelector(".book-list");
-  const bookTitle = document.querySelector("#title");
-  const bookAuthor = document.querySelector("#author");
-  const addBtn = document.querySelector("#submit");
+import Book from './book.js';
 
-  class Book {
-    constructor(title, author) {
-      this.title = title;
-      this.author = author;
-    }
-  }
+window.addEventListener('DOMContentLoaded', () => {
+  const bookSection = document.querySelector('.book-list');
+  const bookTitle = document.querySelector('#title');
+  const bookAuthor = document.querySelector('#author');
+  const addBtn = document.querySelector('#submit');
 
   class Library {
     constructor() {
-      this.library = JSON.parse(localStorage.getItem("book-collection")) || [];
+      this.library = JSON.parse(localStorage.getItem('book-collection')) || [];
     }
+
     addBook(bookTitle, bookAuthor) {
       const selectedBook = new Book(bookTitle.value, bookAuthor.value);
       this.library.push(selectedBook);
@@ -22,18 +18,18 @@ window.addEventListener("DOMContentLoaded", () => {
     }
 
     createBook() {
-      bookSection.innerHTML = "";
+      bookSection.innerHTML = '';
       for (let i = 0; i < this.library.length; i += 1) {
-        const bookContainer = document.createElement("div");
-        bookContainer.setAttribute("class", "container");
-        const bookDescription = document.createElement("h2");
-        bookDescription.setAttribute("class", "width");
-        const deleteBtn = document.createElement("button");
-        deleteBtn.setAttribute("class", "deletebtn");
-        deleteBtn.setAttribute("data", i);
+        const bookContainer = document.createElement('div');
+        bookContainer.setAttribute('class', 'container');
+        const bookDescription = document.createElement('h2');
+        bookDescription.setAttribute('class', 'width');
+        const deleteBtn = document.createElement('button');
+        deleteBtn.setAttribute('class', 'deletebtn');
+        deleteBtn.setAttribute('data', i);
 
         bookDescription.textContent = `${this.library[i].title} by ${this.library[i].author}`;
-        deleteBtn.textContent = "Delete";
+        deleteBtn.textContent = 'Delete';
 
         bookContainer.appendChild(bookDescription);
         bookContainer.appendChild(deleteBtn);
@@ -43,29 +39,29 @@ window.addEventListener("DOMContentLoaded", () => {
     }
 
     deleteBook() {
-      [...document.querySelectorAll(".deletebtn")].map((element) => {
-        const elementIndex = parseInt(element.getAttribute("data"), 10);
-        element.addEventListener("click", () => {
+      [...document.querySelectorAll('.deletebtn')].forEach((element) => {
+        const elementIndex = parseInt(element.getAttribute('data'), 10);
+        element.addEventListener('click', () => {
           this.library.splice(elementIndex, 1);
-          localStorage.setItem("book-collection", JSON.stringify(this.library));
+          localStorage.setItem('book-collection', JSON.stringify(this.library));
           this.createBook();
         });
       });
     }
   }
 
-  let myLibrary = new Library();
+  const myLibrary = new Library();
 
   myLibrary.createBook();
 
-  addBtn.addEventListener("click", (e) => {
+  addBtn.addEventListener('click', (e) => {
     e.preventDefault();
-    if (bookTitle.value === "" || bookAuthor.value === "") {
+    if (bookTitle.value === '' || bookAuthor.value === '') {
       return;
     }
     myLibrary.addBook(bookTitle, bookAuthor);
-    localStorage.setItem("book-collection", JSON.stringify(myLibrary.library));
-    bookTitle.value = "";
-    bookAuthor.value = "";
+    localStorage.setItem('book-collection', JSON.stringify(myLibrary.library));
+    bookTitle.value = '';
+    bookAuthor.value = '';
   });
 });
